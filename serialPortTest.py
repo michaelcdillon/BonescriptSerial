cf7e47e0eac9aa05d92411b2a3e4c496c422b597
Michael Dillon <michaelcdillon@me.com>
2012-03-30 02:09:17 +0000

    added the file write ops for enabling the uart mux

--- serialPortTest.py
+++ serialPortTest.py
@@ -1,4 +1,13 @@
-import serial
+import serial, os
+import sys
+
+txFd = open ('/sys/kernel/debug/omap_mux/uart1_txd', 'wb')
+txFd.write ("0")
+txFd.close ()
+
+rxFd = open ('/sys/kernel/debug/omap_mux/uart1_rxd', 'wb')
+rxFd.write ("%X" % (0 | (1<<5)))
+rxFd.close ()
 
 ser = serial.Serial ('/dev/ttyO1', 9600)
 
